f94b820536f77f37b10ef7eaf2fd896a9571389f
Edwin153 <144918435+Edwin153@users.noreply.github.com>
2023-10-02 00:25:25 +0300

    Update hello world.cpp

--- hello world.cpp
+++ hello world.cpp
@@ -18,8 +18,8 @@ int main()
     cout<<"i am ";
     cout<< age<<"years old" ;*/
 
-    cout<<" Welcome to the wonderful world of c++! \n ";
-    cout<<"This is my first C++ output statement \n";
+    cout<<" Welcome to the wonderful world of C++! \n ";
+    cout<<" This is my first C++ output statement \n";
     cout<<" Soon, I will learn how to input data into my program ";
 
 
